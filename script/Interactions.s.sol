fc7fc9c7e110c0d05161d80205b70e860acc164e
deepakagashe <agashedeepak55@gmail.com>
2024-05-25 19:12:12 +0530

    First contract

---
+++ script/Interactions.s.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {Script,console} from "../lib/forge-std/src/Script.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundFundMe is Script {
+    uint256 constant SEND_VALUE = 0.01 ether;
+    function fundFundMe(address mostRecentDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+    }
+    function run() external {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        fundFundMe(mostRecentDeployed);
+    }
+}
+
+contract WithdrawFundMe is Script {
+     function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        withdrawFundMe(mostRecentlyDeployed);
+    }
+}
